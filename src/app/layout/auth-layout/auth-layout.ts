8e310abe6f486b16f24ec1d2dad235d70be23347
spacecodee <spacecodee@gmail.com>
2025-10-08 17:16:49 -0500

    feat(auth): implement header and footer layout components
    Add reusable header and footer components to the authentication layout.
    This enhances the structure and maintainability of the auth pages by
    centralizing the layout logic.

--- src/app/layout/auth-layout/auth-layout.ts
+++ src/app/layout/auth-layout/auth-layout.ts
@@ -1,15 +1,14 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FooterLayout } from './footer-layout/footer-layout';
+import { HeaderLayout } from './header-layout/header-layout';
 
 @Component({
   selector: 'app-auth-layout',
-  imports: [
-    RouterOutlet
-  ],
+  imports: [RouterOutlet, HeaderLayout, FooterLayout],
   templateUrl: './auth-layout.html',
   styleUrl: './auth-layout.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AuthLayout {
-
 }
