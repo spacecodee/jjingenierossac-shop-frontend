56198b5619002fdb1d99c220b64a760ede686502
spacecodee <spacecodee@gmail.com>
2025-10-08 13:40:11 -0500

    refactor(auth): simplify HTML structure and remove unused CSS
    Refactor the authentication component by streamlining the HTML layout
    and removing unnecessary CSS classes. This improves readability and
    maintains a cleaner codebase.

--- src/app/features/auth/ui/forms/login-form-component/login-form-component.ts
+++ src/app/features/auth/ui/forms/login-form-component/login-form-component.ts
@@ -1,21 +1,13 @@
-import { Component, computed } from '@angular/core';
+import { Component } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { RouterLink } from '@angular/router';
 import { HlmButtonImports } from '@spartan-ng/helm/button';
-import { HlmCardImports } from '@spartan-ng/helm/card';
 import { HlmInputImports } from '@spartan-ng/helm/input';
 import { HlmLabelImports } from '@spartan-ng/helm/label';
 
 @Component({
   selector: 'app-login-form',
-  imports: [
-    ReactiveFormsModule,
-    RouterLink,
-    HlmInputImports,
-    HlmButtonImports,
-    HlmCardImports,
-    HlmLabelImports,
-  ],
+  imports: [ReactiveFormsModule, RouterLink, HlmInputImports, HlmButtonImports, HlmLabelImports],
   templateUrl: './login-form-component.html',
   styleUrl: './login-form-component.css',
 })
@@ -23,10 +15,6 @@ export class LoginFormComponent {
   loginForm = new FormGroup({
     email: new FormControl('', [Validators.required, Validators.email]),
     password: new FormControl('', [Validators.required]),
-  });
-
-  isFormValid = computed(() => {
-    return this.loginForm.valid;
   });
 
   onSubmit(): void {
