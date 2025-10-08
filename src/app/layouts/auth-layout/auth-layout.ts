449a8ff345581cea292d45ddb6809197b969b055
spacecodee <spacecodee@gmail.com>
2025-10-08 08:43:52 -0500

    feat(shared/components): add auth layout component
    Introduce a new authentication layout component with a router outlet.
    This component serves as a container for authentication-related views
    and uses OnPush change detection for improved performance.

---
+++ src/app/layouts/auth-layout/auth-layout.ts
@@ -0,0 +1,15 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-auth-layout',
+  imports: [
+    RouterOutlet
+  ],
+  templateUrl: './auth-layout.html',
+  styleUrl: './auth-layout.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class AuthLayout {
+
+}
